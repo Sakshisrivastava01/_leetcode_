c9fc999ab5baa1630327cebc030797c19fd8ba94
Sakshi19819 <146098991+Sakshisrivastava01@users.noreply.github.com>
2026-05-12 11:29:07 +0530

    Time: 30 ms (23.08%) | Memory: 113.9 MB (67.31%) - LeetSync

--- 1784-minimum-initial-energy-to-finish-tasks/minimum-initial-energy-to-finish-tasks.java
+++ 1784-minimum-initial-energy-to-finish-tasks/minimum-initial-energy-to-finish-tasks.java
@@ -1,46 +1,30 @@
-import java.util.*;
-
 class Solution {
-
-    public boolean canFinish(int[][] tasks, int energy) {
-
+    public boolean isFinish(int[][] tasks, int energy) {
         for (int[] task : tasks) {
-
-            int actual = task[0];
-            int minimum = task[1];
-
-            if (energy < minimum) {
+ int actual = task[0];
+     int minimum = task[1];
+        if (energy < minimum) {
                 return false;
             }
-
             energy -= actual;
         }
-
         return true;
     }
-
     public int minimumEffort(int[][] tasks) {
-
-        Arrays.sort(tasks, (a, b) ->
+ Arrays.sort(tasks, (a, b) ->
                 (b[1] - b[0]) - (a[1] - a[0]));
-
         int low = 1;
         int high = 1000000000;
-
         int ans = high;
-
         while (low <= high) {
-
-            int mid = low + (high - low) / 2;
-
-            if (canFinish(tasks, mid)) {
-                ans = mid;
-                high = mid - 1;
+       int mid = low + (high - low) / 2;
+            if (isFinish(tasks, mid)) {
+          ans = mid;
+       high = mid - 1;
             } else {
-                low = mid + 1;
+                 low = mid + 1;
             }
         }
-
-        return ans;
+return ans;
     }
 }
